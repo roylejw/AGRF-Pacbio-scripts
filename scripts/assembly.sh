4a5a123f8a860c1a3fa12c9d2e0cf56028ccdef6
roylejw <jack.royle@outlook.com>
2023-06-09 09:07:14 +1000

    Update assembly.sh

--- scripts/assembly.sh
+++ scripts/assembly.sh
@@ -123,47 +123,89 @@ echo "Starting Hifiasm. Go get a coffee or 10."
 
 if [ "$hic_confirm" == "yes" ]; then
 	/mnt/efs/fs1/hifiasm/hifiasm -o "$sample".hic.asm -t 48 --hg-size "$genome_size" --h1 /home/ubuntu/"$hic1" --h2 /home/ubuntu/"$hic2" /home/ubuntu/combined.fastq 2> "$sample".log
+	awk '/^S/{print ">"$2;print $3}' "$sample".hic.asm.hic.p_ctg.gfa > "$sample".primary.fasta
+	awk '/^S/{print ">"$2;print $3}' "$sample".hic.asm.hic.hap1.p_ctg.gfa > "$sample".hap1.fasta
+	awk '/^S/{print ">"$2;print $3}' "$sample".hic.asm.hic.hap2.p_ctg.gfa > "$sample".hap2.fasta
+
+	source ~/miniconda3/etc/profile.d/conda.sh
+
+	if [ ! -d ~/miniconda3/envs/quast ]; then
+		cp /mnt/efs/fs1/resources/quast_share.yml .
+		conda env create --file quast_share.yml
+	fi
+
+	conda activate quast
+	echo "Starting QUAST."
+
+	quast --large --no-snps --plots-format png -t 48 "$sample".primary.fasta "$sample".hap1.fasta "$sample".hap2.fasta
+
+	echo "QUAST Finished."
+
+	conda deactivate
+
+	if [ ! -d ~/miniconda3/envs/busco ]; then
+		cp /mnt/efs/fs1/resources/busco_share.yml .
+		conda env create --file busco_share.yml
+	fi
+
+	conda activate busco
+
+	echo "Starting BUSCO."
+
+	busco -i "$sample".primary.fasta --auto-lineage-euk -o "$sample".primary_out -m genome -c 48
+	busco -i "$sample".hap1.fasta --auto-lineage-euk -o "$sample".hap1_out -m genome -c 48
+	busco -i "$sample".hap2.fasta --auto-lineage-euk -o "$sample".hap2_out -m genome -c 48
+
+	echo "BUSCO Finished."
+
+	conda deactivate
+
+	if [ -e "$sample".hic.asm.hic.p_ctg.gfa ]; then
+		aws ec2 stop-instances --instance-ids "$instanceid"
+	fi
+	
 else
-	/mnt/efs/fs1/hifiasm/hifiasm -o "$sample".hic.asm -t 48 --hg-size "$genome_size" /home/ubuntu/combined.fastq 2> "$sample".log
-fi
 
-awk '/^S/{print ">"$2;print $3}' "$sample".hic.asm.hic.p_ctg.gfa > "$sample".primary.fasta
-awk '/^S/{print ">"$2;print $3}' "$sample".hic.asm.hic.hap1.p_ctg.gfa > "$sample".hap1.fasta
-awk '/^S/{print ">"$2;print $3}' "$sample".hic.asm.hic.hap2.p_ctg.gfa > "$sample".hap2.fasta
+	/mnt/efs/fs1/hifiasm/hifiasm -o "$sample".asm -t 48 --hg-size "$genome_size" /home/ubuntu/combined.fastq 2> "$sample".log
+	
+	awk '/^S/{print ">"$2;print $3}' "$sample".hic.asm.bp.p_ctg.gfa > "$sample".primary.fasta
+	awk '/^S/{print ">"$2;print $3}' "$sample".hic.asm.bp.hap1.p_ctg.gfa > "$sample".hap1.fasta
+	awk '/^S/{print ">"$2;print $3}' "$sample".hic.asm.bp.hap2.p_ctg.gfa > "$sample".hap2.fasta
 
-source ~/miniconda3/etc/profile.d/conda.sh
+	source ~/miniconda3/etc/profile.d/conda.sh
 
-if [ ! -d ~/miniconda3/envs/quast ]; then
-	cp /mnt/efs/fs1/resources/quast_share.yml .
-	conda env create --file quast_share.yml
-fi
+	if [ ! -d ~/miniconda3/envs/quast ]; then
+		cp /mnt/efs/fs1/resources/quast_share.yml .
+		conda env create --file quast_share.yml
+	fi
 
-conda activate quast
-echo "Starting QUAST."
+	conda activate quast
+	echo "Starting QUAST."
 
-quast --large --no-snps --plots-format png -t 48 "$sample".primary.fasta "$sample".hap1.fasta "$sample".hap2.fasta
+	quast --large --no-snps --plots-format png -t 48 "$sample".primary.fasta "$sample".hap1.fasta "$sample".hap2.fasta
 
-echo "QUAST Finished."
+	echo "QUAST Finished."
 
-conda deactivate
+	conda deactivate
 
-if [ ! -d ~/miniconda3/envs/busco ]; then
-	cp /mnt/efs/fs1/resources/busco_share.yml .
-	conda env create --file busco_share.yml
-fi
+	if [ ! -d ~/miniconda3/envs/busco ]; then
+		cp /mnt/efs/fs1/resources/busco_share.yml .
+		conda env create --file busco_share.yml
+	fi
 
-conda activate busco
+	conda activate busco
 
-echo "Starting BUSCO."
+	echo "Starting BUSCO."
 
-busco -i "$sample".primary.fasta --auto-lineage-euk -o "$sample".primary_out -m genome -c 48
-busco -i "$sample".hap1.fasta --auto-lineage-euk -o "$sample".hap1_out -m genome -c 48
-busco -i "$sample".hap2.fasta --auto-lineage-euk -o "$sample".hap2_out -m genome -c 48
+	busco -i "$sample".primary.fasta --auto-lineage-euk -o "$sample".primary_out -m genome -c 48
+	busco -i "$sample".hap1.fasta --auto-lineage-euk -o "$sample".hap1_out -m genome -c 48
+	busco -i "$sample".hap2.fasta --auto-lineage-euk -o "$sample".hap2_out -m genome -c 48
 
-echo "BUSCO Finished."
+	echo "BUSCO Finished."
 
-conda deactivate
+	conda deactivate
 
-if [ -e "$sample".hic.asm.hic.p_ctg.gfa ]; then
-	aws ec2 stop-instances --instance-ids "$instanceid"
+	if [ -e "$sample".hic.asm.hic.p_ctg.gfa ]; then
+		aws ec2 stop-instances --instance-ids "$instanceid"
+	fi
 fi
